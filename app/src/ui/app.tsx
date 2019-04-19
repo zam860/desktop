b574c9c9f570a9e4ec730b773050b5366cd89f32
Brendan Forster <github@brendanforster.com>
2019-04-19 10:53:16 -0300

    use the app menu bar for Linux instead of the default chrome (#139)

--- app/src/ui/app.tsx
+++ app/src/ui/app.tsx
@@ -1061,8 +1061,8 @@ export class App extends React.Component<IAppProps, IAppState> {
    * on Windows.
    */
   private renderAppMenuBar() {
-    // We only render the app menu bar on Windows
-    if (!__WIN32__) {
+    // We render the app menu bar on Windows and Linux
+    if (__DARWIN__) {
       return null
     }
 
@@ -1121,13 +1121,16 @@ export class App extends React.Component<IAppProps, IAppState> {
     // When we're in full-screen mode on Windows we only need to render
     // the title bar when the menu bar is active. On other platforms we
     // never render the title bar while in full-screen mode.
+
+    const appMenuPlatform = __WIN32__ || __LINUX__
+
     if (inFullScreen) {
-      if (!__WIN32__ || !menuBarActive) {
+      if (!appMenuPlatform || !menuBarActive) {
         return null
       }
     }
 
-    const showAppIcon = __WIN32__ && !this.state.showWelcomeFlow
+    const showAppIcon = appMenuPlatform && !this.state.showWelcomeFlow
 
     return (
       <TitleBar
